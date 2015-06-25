0203504eb464a28c28f2c1c681d791b9d76e5ad3
NesterovichEugene <nesterowich.e@gmail.com>
2015-06-25 21:30:19 +0300

    Calendar

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace EasternCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.yearTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // yearTextBox
+            // 
+            this.yearTextBox.BackColor = System.Drawing.Color.Tan;
+            this.yearTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.yearTextBox.Location = new System.Drawing.Point(136, 81);
+            this.yearTextBox.MaxLength = 4;
+            this.yearTextBox.Name = "yearTextBox";
+            this.yearTextBox.Size = new System.Drawing.Size(100, 20);
+            this.yearTextBox.TabIndex = 0;
+            this.yearTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.yearTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.IsDigit_KeyPress);
+            // 
+            // showButton
+            // 
+            this.showButton.BackColor = System.Drawing.Color.BurlyWood;
+            this.showButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.showButton.Location = new System.Drawing.Point(149, 116);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Показать";
+            this.showButton.UseVisualStyleBackColor = false;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(55, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 221);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(149, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Укажите год:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.yearTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Восточный календарь";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox yearTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*«Восточный календарь». В восточном календаре принят 60летний цикл, состоящий из 12-
+летних подциклов, обозначаемых названиями цвета: зеленый, красный, желтый, белый и
+голубой. При этом каждый цвет следует по два года подряд. В каждом подцикле годы носят
+названия животных: крысы, коровы, тигра, зайца, дракона, змеи, лошади, овцы, обезьяны,
+петуха, собаки и свиньи. 4 год нашей эры — начало цикла: «год зеленой крысы». Написать
+приложение-визуализатор по номеру года выводится соответствующая картинка на соответсвующем фоне*/
+
+namespace EasternCalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*     
+         Bitmap Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\rat.png");
+         pictureBox1.Image = (Image)Image;
+         pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage ;
+            */
+        private void IsDigit_KeyPress(object sender, KeyPressEventArgs e) //запрет ввода букв
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            if (yearTextBox.Text != "")
+            {
+                int year = int.Parse(yearTextBox.Text);
+                year = year - (year - 4) / 60 * 60;
+                int yearColor, yearAnimal;
+                
+                if(year>=4)
+                {
+                    //Color
+                    yearColor = year - (year / 10) * 10;
+                    
+                    var green = "#00FF7F";
+                    var myGreen = ColorTranslator.FromHtml(green);
+                    var red = "#EE2C2C";
+                    var myRed = ColorTranslator.FromHtml(red);
+                    var yellow = "#FFF68F";
+                    var myYellow = ColorTranslator.FromHtml(yellow);
+                    var white = "#FFFAF0";
+                    var myWhite = ColorTranslator.FromHtml(white);
+                    var blue = "#1E90FF";
+                    var myBlue = ColorTranslator.FromHtml(blue);
+
+                    if (yearColor == 4 || yearColor == 5) pictureBox1.BackColor = myGreen;
+                    else if (yearColor == 6 || yearColor == 7) pictureBox1.BackColor = myRed;
+                    else if (yearColor == 8 || yearColor == 9) pictureBox1.BackColor = myYellow;
+                    else if (yearColor == 0 || yearColor == 1) pictureBox1.BackColor = myWhite;
+                    else pictureBox1.BackColor = myBlue;
+
+                    //Animal
+                    yearAnimal = year - (year - 4 )/ 12* 12 - 4;
+
+                    Bitmap Image=null;
+                    if (yearAnimal == 0) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\rat.png");
+                    if (yearAnimal == 1) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\cow.png");
+                    if (yearAnimal == 2) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\tiger.png");
+                    if (yearAnimal == 3) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\rabbit.png");
+                    if (yearAnimal == 4) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\dragon.png");
+                    if (yearAnimal == 5) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\snake.png");
+                    if (yearAnimal == 6) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\horse.png");
+                    if (yearAnimal == 7) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\sheep.png");
+                    if (yearAnimal == 8) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\monkey.png");
+                    if (yearAnimal == 9) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\cock.png");
+                    if (yearAnimal == 10) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\dog.png");
+                    if (yearAnimal == 11) Image = new Bitmap("D:\\EasternCalendar\\EasternCalendar\\resourses\\pig.png");
+
+                    pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                    pictureBox1.Image = (Image)Image;
+                }
+            }
+            else
+            {
+                pictureBox1.BackColor = Color.Transparent;
+                pictureBox1.Image = null;
+            }
+        }
+    }
+}
